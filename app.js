bd6ebd9765b058cf41d10e966e9f3f62419bcd34
vovanolo <vpankiv@softserveinc.com>
2023-05-30 17:44:03 +0300

    lesson1

---
+++ app.js
@@ -0,0 +1,86 @@
+// console.log("hello world!");
+
+// console.log("hello world!");
+
+
+// let const (var)
+
+// let message = "hello world!";
+
+// const message = "hello world!";
+
+// message = 34;
+
+// let name;
+
+// name = 'hello';
+
+// console.log(message);
+
+// const name;
+// name = 'hello';
+
+// let message = "hello world!"; // created a variable
+
+// this is comment
+
+// let name = "Vova";
+// let number = 23;
+
+// name = number;
+
+// console.log(name);
+
+// let message = " '' ";
+
+// const user = {
+//     name: "volodymyr",
+//     age: 3,
+//     online: true,
+//     city: undefined,
+//     money: null
+// }
+
+// // user = 3;
+// console.log(user);
+
+// "username123*.."
+
+// let name = "username";
+
+// name = 40;
+// name = true;
+// name = null;
+
+// console.log(typeof(name));
+
+// Вивід інформації в модальне вікно
+
+// alert("Hello");
+
+// Зчитування інформації
+
+// let name = prompt("Please enter your name");
+
+// console.log("Hello new user " + name);
+
+// console.log(`Hello new user ${name}`);
+
+// Підтвердження інформації
+
+// let ok = confirm("Are you ok?");
+
+// console.log(ok);
+
+// let name = "Volodymyr";
+// let surname = "Pankiv";
+
+// console.log("hello my name is "+ name + " and surname is "+ surname);
+
+
+
+
+
+
+
+
